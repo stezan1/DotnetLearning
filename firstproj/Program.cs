e6ee2b3b703c763c8e9db41a2d8017871178bf93
stezan1 <shahuchandan537@gmail.com>
2024-09-04 08:16:30 +0545

    learned datatypes

--- firstproj/Program.cs
+++ firstproj/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+datatypes
+
+//integer
+byte, short, ushort, int, uint, long, ulong
+
+//floating point
+float, double, decimal
+
+//text
+char,string
+
+//boolean
+bool 
+
+
+var
+//to allow nullable = datatype?  eg: int? i;
+
+*/
+
+
 Console.WriteLine("Hello, World!");
